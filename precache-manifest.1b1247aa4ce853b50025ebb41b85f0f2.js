97625393c34854bd9ccc526b3524d26d794fdbc9
MaxNoka <maxnoka@me.com>
2020-08-10 18:57:05 +0100

    Updates

---
+++ precache-manifest.1b1247aa4ce853b50025ebb41b85f0f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db274b0b528f647946b4cf407b6fd5a",
+    "url": "/warehouse-vis/index.html"
+  },
+  {
+    "revision": "7dfd1750245ec1caea06",
+    "url": "/warehouse-vis/static/css/main.71173543.chunk.css"
+  },
+  {
+    "revision": "e9a484981a661791edc3",
+    "url": "/warehouse-vis/static/js/2.04e23a4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/warehouse-vis/static/js/2.04e23a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfd1750245ec1caea06",
+    "url": "/warehouse-vis/static/js/main.010167be.chunk.js"
+  },
+  {
+    "revision": "49e870f354276d2e8860",
+    "url": "/warehouse-vis/static/js/runtime-main.83b747ad.js"
+  }
+]);
